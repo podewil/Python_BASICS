2e2476fb5c57ea6f59b87fbe5a5b7d3bfeccdeeb
CHANG <changb.joo@gmail.com>
2017-05-10 13:22:44 -0400

    Add files via upload

--- Basic_for.py
+++ Basic_for.py
@@ -1,10 +1,10 @@
 ABCD = ['A_apple', 'B_Bravo', 'C_Charlie', 'D_Dad']
 
-i = "something"
+i = "something"     # i is initialized with string variable
 print(i)
 
 for i in ABCD:
-    print(i)
-    print(len(i))
+    print(i)        # Prints string element
+    print(len(i))   # Prints out length of string
 
-print(i)
+print(i)            # Note! prints out D_Dad
