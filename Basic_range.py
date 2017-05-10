ba1e6e566a40fbec1664c7f044116f7c0f271981
CHANG <changb.joo@gmail.com>
2017-05-10 13:37:09 -0400

    Add files via upload

---
+++ Basic_range.py
@@ -0,0 +1,8 @@
+'''
+range(stop)                     # Starts from 0 with '1' step size
+range(start, stop, step)
+'''
+
+print(list(range(5)))           # Prints out [0, 1, 2, 3, 4]
+
+print(list(range(10, 50, 5)))   # Prints out [10, 15, 20, 25, 30, 35, 40, 45]
